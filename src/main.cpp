3057d8a72069a29add0bc20c664f4be60c55e2ff
Pavel Ilin <ilin.pa@gmail.com>
2015-01-31 15:12:09 +0200

    Initial

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <Windows.h>
+
+
+int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd) {
+	return 0;
+}
