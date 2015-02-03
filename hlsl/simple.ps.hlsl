534b9dba952d33ee0db81a446e3728c74a24fa78
Pavel Ilin <ilin.pa@gmail.com>
2015-02-03 19:43:02 +0200

    Make colors slightly better

--- hlsl/simple.ps.hlsl
+++ hlsl/simple.ps.hlsl
@@ -4,6 +4,9 @@ float4 main(sPSSimple pin) : SV_TARGET
 {
 
 	float4 clr = float4(1.0f, 1.0f, 1.0f, 1.0f);
-	clr = pin.clr;
+	float3 rgb = pin.clr.rgb;
+	rgb = rgb * 0.55 + 0.225;
+	clr.rgb = rgb;
+	clr.a = pin.clr.a;
 	return clr;
 }
